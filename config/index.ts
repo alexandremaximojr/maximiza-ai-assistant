8a97cd5204f9aa92e556d244616075b734026683
Alexandre Maximo <128348448+alexandremaximojr@users.noreply.github.com>
2025-02-26 03:51:39 -0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,9 @@
 import type { AppInfo } from '@/types/app'
 
 // Configurações da API Dify (inseridas diretamente para teste)
-export const APP_ID = 'gvbBTYbSYGKotY5j'
+export const APP_ID = '7c6ef1c4-481a-4e61-bca4-34e1c6de438d'
 export const API_KEY = 'app-4hqurh1ytQvaRbh2vuJFrE9H' // Substitua pela sua API key real
-export const API_URL = 'https://udify.app'
+export const API_URL = 'http://dify-dify.uhds1z.easypanel.host/v1'
 export const IS_WORKFLOW = false
 
 // Informações do aplicativo
